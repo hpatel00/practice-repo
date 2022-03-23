27c43d7e7189c580000dd51d67b6bb4e847567a1
hpatel00 <hpatel2356@gmail.com>
2022-03-23 14:45:22 -0400

    Automated roboshop error script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,16 +15,20 @@ systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
 Check_Stat $?
 
 Print "Create User"
-grep temp /var/log/mysqld.log &>>${LOG_FILE} && mysql_secure_installation &>>${LOG_FILE} &&
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+mysql -uroot -p"DEFAULT_ROOT_PASSWORD" </tmp/rootpass.sql
 
-Print "Download Database Schema"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
-Check_Stat $?
-
-Print "Extract Database Schema"
-cd /tmp && unzip mysql.zip &>>${LOG_FILE}
-Check_Stat $?
-
-Print "Load Database Schema"
-cd mysql-mainmysql && mysql -u root -pRoboShop@1 <shipping.sql &>>${LOG_FILE}
-Check_Stat $?
+#&>>${LOG_FILE} && mysql_secure_installation &>>${LOG_FILE} &&
+#
+#Print "Download Database Schema"
+#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
+#Check_Stat $?
+#
+#Print "Extract Database Schema"
+#cd /tmp && unzip mysql.zip &>>${LOG_FILE}
+#Check_Stat $?
+#
+#Print "Load Database Schema"
+#cd mysql-mainmysql && mysql -u root -pRoboShop@1 <shipping.sql &>>${LOG_FILE}
+#Check_Stat $?
