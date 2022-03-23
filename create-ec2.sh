a5e781607eda4cdc5101c3266d34588a474fa0d6
hpatel00 <hpatel2356@gmail.com>
2022-03-23 13:07:41 -0400

    Added a function to create DNS record while creating instance

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,14 +6,19 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z0150156UD0J2BIEAJM9"
 
 CREATE_EC2() {
-  aws ec2 run-instances \
+  PRIVATE_IP=$(aws ec2 run-instances \
         --image-id ${AMI_ID} \
         --instance-type t2.micro \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
         --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-        --security-group-ids ${SGID}
+        --security-group-ids ${SGID} \
+        | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/COMPONENT/${COMPONENT}" -e "s/IPADDRESS/${PRIVATE_IP}" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
