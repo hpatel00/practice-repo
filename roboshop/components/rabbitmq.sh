7cb5e7a2811231a7e534bdf6618dbbaabe34c0be
hpatel00 <hpatel2356@gmail.com>
2022-03-23 16:04:49 -0400

    Automated rabbitmq file

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -2,16 +2,12 @@
 
 source components/common.sh
 
-Print "Install Erlang Dependency"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG_FILE
-Check_Stat $?
-
 Print "Setup YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
 Check_Stat $?
 
-Print "Install RabbitMQ"
-yum install rabbitmq-server -y &>>$LOG_FILE
+Print "Install Erlang Dependency"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
 Check_Stat $?
 
 Print "Start RabbitMQ"
