259dde8fb47d42c0c4003d4937aca5c5c59bb8e2
hpatel00 <hpatel2356@gmail.com>
2022-03-23 12:24:26 -0400

    Added a function to automate ec2 creation

---
+++ create-ec2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "Input Machine Name is Needed"
+  exit 1
+fi
+
+COMPONENT=$1
+
+
+CREATE_EC2() {
+
+aws ec2 run-instances \
+                      --image-id ${AMI_ID} \
+                      --instance-type t2.micro \
+                      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+                      --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop" \
+                      --security-group-ids ${SGID} \
+}
+
+$AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+$SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+if [ "$1" == "all" ]; then
+  for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
+    COMPONENT=$component
+    CREATE_EC2
+  done
+else
+    CREATE_EC2
+fi
