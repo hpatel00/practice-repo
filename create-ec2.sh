6a03815141d4d85d633563cff2772666c6304904
hpatel00 <hpatel2356@gmail.com>
2022-03-23 12:37:20 -0400

    Added a function to automate ec2 creation

--- create-ec2.sh
+++ create-ec2.sh
@@ -7,19 +7,17 @@ fi
 
 COMPONENT=$1
 
-
 CREATE_EC2() {
-
-aws ec2 run-instances \
+  aws ec2 run-instances \
                       --image-id ${AMI_ID} \
                       --instance-type t2.micro \
                       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
                       --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop" \
-                      --security-group-ids ${SGID} \
+                      --security-group-ids ${SGID}
 }
 
-$AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-$SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 if [ "$1" == "all" ]; then
   for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
